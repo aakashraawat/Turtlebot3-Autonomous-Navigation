656906962bdbb761bf2392c6ec6884a86134f094
Aakash Rawat <aakashraawat@icloud.com>
2023-12-18 22:58:47 +0100

    completed

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/aakashrawat/nav_ws/src
+# Build directory: /home/aakashrawat/nav_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("auto_nav")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
